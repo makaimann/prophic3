3a4a76383a91a9c9d11e5c4efef5f40d5691e7f3
Makai Mann <makaim@stanford.edu>
2019-07-29 16:14:36 -0700

    Update array_abstract docstrings

--- array-sim/array_abstract.h
+++ array-sim/array_abstract.h
@@ -10,11 +10,12 @@
 
 namespace array_utils {
 /**
- * Takes a formula containing arrays removes array-writes by introducing fresh
- * array symbols Returns the new formula and a list of the top-level array
- * equality constraints that contain only a single store.
+ * Takes a list of formulae containing arrays and removes array-writes by introducing fresh
+ * array symbols.
  *
- * Modifies the TermList in place
+ * Modifies the TermList in place and returns a list of the top-level array equality
+ * constraints that contain only a single store. Those are to be used for enumerating
+ * lemmas.
  */
 ic3ia::TermList flatten_arrays(msat_env env, ic3ia::TermList &terms);
 
@@ -22,7 +23,8 @@ ic3ia::TermList flatten_arrays(msat_env env, ic3ia::TermList &terms);
  * Replaces reads and array equalities with uninterpreted functions
  * Should be called after flatten_arrays
  *
- * Modifies the TermList in place
+ * Modifies the TermList in place and returns a vector with 3 set elements:
+ *   set of indices, set of abstracted array equalities, set of abstracted array reads
  */
 std::vector<ic3ia::TermSet> abstract(msat_env env, ic3ia::TermList &term);
 } // namespace array_utils
