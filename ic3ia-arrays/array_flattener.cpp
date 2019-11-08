81e82277d6a45bf0dd625bbfb1283405db3718fa
Ahmed Irfan <irfan@cs.stanford.edu>
2019-11-08 14:01:09 -0800

    fix: do init and prop first in the flattener

--- ic3ia-arrays/array_flattener.cpp
+++ ic3ia-arrays/array_flattener.cpp
@@ -25,7 +25,6 @@ void ArrayFlattener::do_flattening()
 
     // first do init and property
     msat_term new_init = flatten(orig_ts_.init());
-    msat_term new_trans = flatten(orig_ts_.trans());
     msat_term new_prop = flatten(orig_ts_.prop());
 
     // create state vars for new variables appearing in init and prop
@@ -37,15 +36,18 @@ void ArrayFlattener::do_flattening()
                                 msat_term_get_type(elem.first));
         msat_term vn = msat_make_constant(msat_env_, vn_decl);
         new_state_vars[elem.first] = vn;
+
+	msat_term arr_eq = msat_make_equal(msat_env_, elem.first, new_vars_.at(elem.first));
+	new_init = msat_make_and(msat_env_, new_init, arr_eq);
     }
 
     // current to next
     auto subst = [=](msat_term v) -> msat_term { return new_state_vars.at(v); };
     TermMap next_cache;
 
+    msat_term new_trans = flatten(orig_ts_.trans());
     for (auto elem : new_vars_) {
       msat_term arr_eq = msat_make_equal(msat_env_, elem.first, new_vars_.at(elem.first));
-      new_init = msat_make_and(msat_env_, new_init, arr_eq);
       new_trans = msat_make_and(msat_env_, new_trans, arr_eq);
 
       // add next-state version of invariants (only involves current vars)
