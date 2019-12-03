3f62325c83192481f2b2c0e836001149b6224466
Ahmed Irfan <irfan@cs.stanford.edu>
2019-12-02 21:02:35 -0800

    typo

--- ic3ia-arrays/utils.cpp
+++ ic3ia-arrays/utils.cpp
@@ -234,7 +234,7 @@ Options get_options(int argc, const char **argv)
             ret.use_uf_for_arr_eq = false;
         } else if (a == "-no-hist-eq-preds") {
           ret.use_hist_eq_initial_preds = false;
-        } else if (a == "lazy-array-axioms") {
+        } else if (a == "-lazy-array-axioms") {
 	  ret.lazy_array_axioms = true;
 	} else if (a == "-h" || a == "-help" || a == "--help") {
             std::cout << "USAGE: " << argv[0] << " [OPTIONS] FILENAME.vmt"
